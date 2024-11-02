a67718d56a965e7a5c2882287ed9dfbbf52931cc
Graham <gcoulby@gmail.com>
2024-11-02 14:52:57 +0000

    theres always one thing

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/<REPO_NAME>/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/color-picker/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
